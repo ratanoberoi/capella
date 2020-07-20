ea391245b3fddd6ee9ead0cb87c171e28254dc3b
Ratan Oberoi <65130962+ratanoberoi@users.noreply.github.com>
2020-07-20 19:00:30 -0400

    update

--- tempalate-parts/hero.php
+++ tempalate-parts/hero.php
@@ -14,10 +14,10 @@
             <button>
                 <a href="#">Get Started</a>
             </button>
-        <div class="triangle">
-			<img src="<?php echo esc_attr( get_template_directory_uri() . '/assets/src/images/Triangle.png' ); ?>"
+            <div class="triangle">
+			    <img src="<?php echo esc_attr( get_template_directory_uri() . '/assets/src/images/Triangle.png' ); ?>"
 			     alt="Triangle">
-		</div>
+		    </div>
             <a href="#">Watch the Video</a>
         </div>
     </div>    
