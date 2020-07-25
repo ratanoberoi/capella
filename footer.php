fd461fa3fd81b92689e0f0ce88125f64e18a303b
Ratan Oberoi <65130962+ratanoberoi@users.noreply.github.com>
2020-07-24 21:54:25 -0400

    update

--- footer.php
+++ footer.php
@@ -1,6 +1,16 @@
 <footer class="footer">
-	Footer
-</footer>
+
 <?php wp_footer(); ?>
 </body>
+<div class="footer-left">
+		<div class="logo1">
+			<img src="<?php echo esc_attr( get_template_directory_uri() . '/assets/src/images/Bitmap.png'); ?>"
+				 alt="bitmap">
+		<div class="logo2">
+			<img src="<?php echo esc_attr( get_template_directory_uri() . '/assets/src/images/Productly.png' ); ?>"
+			     alt="Productly">
+		</div>
+
+</div>
 </html>
+</footer>
