2e394de89f890188d2da4fae8650415e8efac1af
Ratan Oberoi <65130962+ratanoberoi@users.noreply.github.com>
2020-08-01 23:12:31 -0400

    update

--- footer.php
+++ footer.php
@@ -2,7 +2,8 @@
 
 <?php wp_footer(); ?>
 </body>
-<div class="footer-left">
+<div class="footer wrapper" >
+	<div class="footer-left">
 		<div class="logo1">
 			<img src="<?php echo esc_attr( get_template_directory_uri() . '/assets/src/images/Bitmap.png'); ?>"
 				 alt="bitmap">
@@ -10,17 +11,23 @@
 			<img src="<?php echo esc_attr( get_template_directory_uri() . '/assets/src/images/Productly.png' ); ?>"
 				 alt="Productly">
 		</div>
-		<h1>Quick Links</h1>
-		<ol>About Us</ol>
-		<ol>Blog</ol>
-		<ol>Contact</ol>
-		<ol>FAQ</ol>
-		<h2>Quick Links</h2>
-		<ol>Disclamer</ol>
-		<ol>Financing</ol>
-		<ol>Privacy Policy</ol>
-		<ol>Terms of Service</ol>
+		<div class="quick links">	
+			<h1>Quick Links</h1>
+			<ol>About Us</ol>
+			<ol>Blog</ol>
+			<ol>Contact</ol>
+			<ol>FAQ</ol>
+		</div>
+		<div class="legal stuff">	
+			<h2>Legal Stuff</h2>
+			<ol>Disclamer</ol>
+			<ol>Financing</ol>
+			<ol>Privacy Policy</ol>
+			<ol>Terms of Service</ol>
+		</div>
+	
 		<h3>knowing you're always on the best energy deal.</h3>
+	</div>
 </div>
 </html>
 </footer>
