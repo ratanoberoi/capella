c9dcbeaad3a6384ab756a995f2635361760b27c3
Ratan Oberoi <65130962+ratanoberoi@users.noreply.github.com>
2020-08-03 22:30:38 -0400

    update

--- footer.php
+++ footer.php
@@ -12,19 +12,21 @@
 </body>
 <div class="footer wrapper" >
 	<div class="row">
-		<div class="columns large-4 medium-6 small-12">
-			<div class="logo1">
-				<img src="<?php echo esc_attr( get_template_directory_uri() . '/assets/src/images/Bitmap.png'); ?>"
-					alt="bitmap">
-			</div>		
-			<div class="logo2">
-				<div class="footer-left">
-					<img src="<?php echo esc_attr( get_template_directory_uri() . '/assets/src/images/Productly.png' ); ?>"
-					alt="Productly">
+		<div class="columns large-4 medium-4 small-12 large-expand">
+			<div class="logos">
+				<div class="logo1">
+					<img src="<?php echo esc_attr( get_template_directory_uri() . '/assets/src/images/Bitmap.png'); ?>"
+						alt="bitmap">
+				</div>		
+				<div class="logo2">
+					<div class="footer-left">
+						<img src="<?php echo esc_attr( get_template_directory_uri() . '/assets/src/images/Productly.png' ); ?>"
+						alt="Productly">
+					</div>
 				</div>
 			</div>
 		</div>
-		<div class="columns large-2 medium-6 small-12">
+		<div class="columns large-2 medium-2 small-12 large-expand">
 			<div class="quick links">	
 				<h1>Quick Links</h1>
 				<ol>About Us</ol>
@@ -33,7 +35,7 @@
 				<ol>FAQ</ol>
 			</div>
 		</div>
-		<div class="columns large-2 medium-6 small-12">
+		<div class="columns large-2 medium-2 small-12 large-expand">
 			<div class="legal stuff">	
 				<h2>Legal Stuff</h2>
 				<ol>Disclamer</ol>
@@ -42,7 +44,7 @@
 				<ol>Terms of Service</ol>
 			</div>
 		</div>
-		<div class="columns large-4 medium-6 small-12">
+		<div class="columns large-4 medium-4 small-12 large-expand">
 			<h3>knowing you're always on the best energy deal.</h3>
 		</div>
 	</div>
